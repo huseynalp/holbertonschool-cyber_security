ac58b647ca6746cd5ab3ffe99c49ce56712ed5a9
huseynalp <huseynalph@gmail.com>
2025-12-15 12:54:57 +0000

    Add sha256

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-hash=$(sha256sum "$1" | cut -d' ' -f1)
-if [ "$hash" = "$2" ]; then echo "$1: OK"; else echo "$1: FAIL"; fi
+if [ "$(sha256sum "$1" | cut -d' ' -f1)" = "$2" ]; then echo "$1: OK"; else echo "$1: FAIL"; fi
