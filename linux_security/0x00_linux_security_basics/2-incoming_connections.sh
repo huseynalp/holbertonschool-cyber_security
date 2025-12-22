b282a2d79d679191a1f5080030b1b479466897a7
huseynalp <huseynalph@gmail.com>
2025-12-22 13:08:18 +0000

    Add script that allow only incoming connections with the TCP

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo iptables -A INPUT -j DROP
-sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo ip6tables -A INPUT -j DROP
-echo "Rules updated"
-echo "Rules updated (v6)"
