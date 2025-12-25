ecce070a054f043659ea1aa377fbbc5d171d55ff
huseynalp <huseynalph@gmail.com>
2025-12-25 11:19:52 +0000

    Add script that extract using whois scan in csv format

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk '/^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Phone Ext:|Fax|Fax Ext:|Email):/{sub(/: /,",",$0);if(/Street,/)$0=$0" ";if(/Ext:,/)sub(/Ext:,/,"Ext:,",$0);print}' | head -c -1 > $1.csv && echo >> $1.csv
+whois $1 | awk '/^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Phone Ext:|Fax|Fax Ext:|Email):/{sub(/: /,",",$0);if(/Street,/)$0=$0" ";if(/Ext:,/)sub(/Ext:,/,"Ext:,",$0);print}END{printf "\n"}' > $1.csv
